394b0f2275007f1ec9d5162bab85e26ae6df6032
Nikolay0107 <nimphonoe@gmail.com>
2020-07-26 21:26:13 +0300

    Version 0.01
    Board display!

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,7 +1,32 @@
 
 #include <iostream>
+using namespace std;
+char matrix[3][3] = { '1','2','3','4','5','6','7','8','9' }; // инициализируем глобальный char массив 3-3, для доски
+
+
+// функция для отрисовки массива matrix
+
+void Draw()
+{
+    cout << "\n   Welcome to game TicTacToe!\n\n" ;
+    //пробегаемся по элементам массива
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "\t";
+        for (int j = 0; j < 3; j++)
+        {
+            cout << matrix[i][j] << "   ";
+        }
+
+        cout << "\n\n";
+    }
+}
+
+
 
 int main()
 {
-    std::cout << "TicTacToe!\n";
+    Draw();
+
+    return 0;
 }
