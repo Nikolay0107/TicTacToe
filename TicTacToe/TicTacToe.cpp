d7367f5943db762d378a9ac4c584406a102b5f1c
Nikolay0107 <nimphonoe@gmail.com>
2020-07-27 14:37:12 +0300

    Update. Bugs fixed!

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,11 +1,11 @@
 
 #include <iostream>
-#include <Windows.h> // подключаем директиву для цвета
+#include <Windows.h> // подключаем директиву для цвета консоли
 using namespace std;
-HANDLE colour = GetStdHandle(STD_OUTPUT_HANDLE); // подключаем для цвета
 
 char matrix[3][3] = { '1','2','3','4','5','6','7','8','9' }; // инициализируем глобальный char массив 3-3, для доски
-char player = 'X'; // переменная для ходов игрока
+char player = 'X'; // переменная для ходов игрока, по умолчанию Х
+int var; //для счетчика ходов и определение ничьей
 
 
 // Функция для смены цвета текста в консоли
@@ -20,7 +20,7 @@ void SetColor(int ForgC)
     //We use csbi for the wAttributes word.
     if (GetConsoleScreenBufferInfo(hStdOut, &csbi))
     {
-        //Mask out all but the background attribute, and add in the forgournd     color
+        //Mask out all but the background attribute, and add in the forgournd color
         wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
         SetConsoleTextAttribute(hStdOut, wColor);
     }
@@ -28,7 +28,7 @@ void SetColor(int ForgC)
 
     /*
     Name         | Value
-                |
+                 |
     Black        |   0
     Blue         |   1
     Green        |   2
@@ -78,6 +78,12 @@ void Draw()
 void Input()
 {
     int a;
+    SetColor(13);
+    cout << "\tIt's ";
+    SetColor(10);
+    cout << player;
+    SetColor(13);
+    cout << " turn." << endl << endl << endl;
     SetColor(14);
     cout << "  Press the number of the field:\t";
     SetColor(11);
@@ -129,12 +135,10 @@ void TogglePlayer()
 
 char Win()
 {
-
     //ПРОВЕРКА ДЛЯ ИГРОКА Х
 
     //проверка для строк
-    
-    if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+   if (matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
         return 'X';
     if (matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X')
         return 'X';
@@ -153,14 +157,9 @@ char Win()
     if (matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X')
         return 'X';
 
-
- 
-
-
-
     //ПРОВЕРКА ДЛЯ ИГРОКА O
 
-     //проверка для строк
+    //проверка для строк
     if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
         return 'O';
     if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
@@ -180,18 +179,19 @@ char Win()
     if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O')
         return 'O';
 
-    return '/';
+    return 'D'; // Для возврата результата "ничья - Draw"
 }
 
 
 int main()
 {
- 
+    var = 0;
     //Делаем бесконечный цикл
     //Фызываем функции ввода хода, отрисовки массива (доски) и смену игрока
     Draw();
     while (1)
     {
+        var++; // увеличиваем для счетчика ходов
         Input();
         Draw();   
         if (Win() == 'X')
@@ -205,6 +205,13 @@ int main()
         {
             SetColor(10); // для цвета текста при выигрыше
             cout << "\tO wins!\n\n";
+            SetColor(14);
+            break;
+        }
+        else if (Win() == 'D' && var == 9)
+        {
+            SetColor(12); // для цвета текста при выигрыше
+            cout << "\tIt's a draw!\n\n";
             SetColor(14);
             break;
         }
