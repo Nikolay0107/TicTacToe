bdfd3869006319d63374b40ec98bdd71e13b7df4
Nikolay0107 <nimphonoe@gmail.com>
2020-07-27 15:36:15 +0300

    Version 1.0
    Works well. Need multifile and less code!

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -93,24 +93,86 @@ void Input()
 
 
     if (a == 1)
-        matrix[0][0] = player;
-    else if (a == 2)
-        matrix[0][1] = player;
-    else if (a == 3)
-        matrix[0][2] = player;
-    else if (a == 4)
-        matrix[1][0] = player;
-    else if (a == 5)
-        matrix[1][1] = player;
-    else if (a == 6)
-        matrix[1][2] = player;
-    else if (a == 7)
-        matrix[2][0] = player;
-    else if (a == 8)
-        matrix[2][1] = player;
-    else if (a == 9)
-        matrix[2][2] = player;
-
+    {
+        if (matrix[0][0] == '1')
+            matrix[0][0] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+     }
+    if (a == 2)
+    {
+        if (matrix[0][1] == '2')
+            matrix[0][1] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 3)
+    {
+        if (matrix[0][2] == '3')
+            matrix[0][2] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 4)
+    {
+        if (matrix[1][0] == '4')
+            matrix[1][0] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 5)
+    {
+        if (matrix[1][1] == '5')
+            matrix[1][1] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 6)
+    {
+        if (matrix[1][2] == '6')
+            matrix[1][2] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 7)
+    {
+        if (matrix[2][0] == '7')
+            matrix[2][0] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 8)
+    {
+        if (matrix[2][1] == '8')
+            matrix[2][1] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
+    if (a == 9)
+    {
+        if (matrix[2][2] == '9')
+            matrix[2][2] = player;
+        else {
+            cout << "Field is already in use. Try again! " << endl;
+            Input();
+        }
+    }
 }
 
 
